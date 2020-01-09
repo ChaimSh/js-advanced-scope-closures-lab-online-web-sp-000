a897c8eb51acf5ecdbc025468e08fc611e9b905f
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-09 03:51:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-function produceDrivingRange(blockRange){
-  return function (begin, end){
-    let start = parseInt( begin );
-    let end = parseInt( end );
-    let distanceToTravel = Math.abs(end - start);
+// function produceDrivingRange(blockRange){
+//   return function (begin, end){
+//     let start = parseInt( begin );
+//     let end = parseInt( end );
+//     let distanceToTravel = Math.abs(end - start);
+//     let difference = blockRange - distanceToTravel;
+// 
+//     if (difference > 0) {
+//       return `within range by ${difference}`
+//     } else {
+//         return `${Math.abs(difference)} blocks out of range`
+//       }
+//     }
+// }
+
+function produceDrivingRange( blockRange ) {
+  return function ( startingBlock, endingBlock ) {
+
+    let start = parseInt( startingBlock );
+    let end = parseInt( endingBlock );
+    let distanceToTravel = Math.abs( end - start );
     let difference = blockRange - distanceToTravel;
 
-    if (difference > 0) {
+    if ( difference > 0 ) {
       return `within range by ${difference}`
     } else {
-        return `${Math.abs(difference)} blocks out of range`
-      }
+      return `${Math.abs(difference)} blocks out of range`
     }
+  }
 }
