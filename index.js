2637c7f47b3fb8cfa4c203c14d2f1e744c0746fe
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-09 03:25:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ function produceDrivingRange(blockRange){
   return function rangeCalculator(begin, end){
     let start = parseInt( begin );
     let end = parseInt( end );
-    let distanceToTravel = Math.abs(end - start)
-    let difference = blockRange - distanceToTravel
+    let distanceToTravel = Math.abs(end - start);
+    let difference = blockRange - distanceToTravel;
 
     if (difference > 0) {
       return `within range by ${difference}`
-      else {
+    } else {
         return `${Math.abs(difference)} blocks out of range`
       }
     }
-  }
 }
